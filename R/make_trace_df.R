9a2f3ee39864a49f18cef0e8249668e7ba44e71c
Ruben Lopez <rjlopez2@gmail.com>
2022-01-04 23:01:56 +0100

    updtae/simplify `make_trace_function`

--- R/make_trace_df.R
+++ R/make_trace_df.R
@@ -11,8 +11,9 @@
 #' 
 make_trace_df <- function(my_file_dir, msg = F){
   
-  # y_px_size = read_tags(my_file_dir) %>% 
-  #   pluck("frame1", "y_resolution")
+  file_id = path_file(my_file_dir)
+  y_px_size = read_tags(my_file_dir) %>%
+    pluck("frame1", "y_resolution")
   
   my_trace_df <- my_file_dir %>%
     ijtiff::read_tif(msg = msg) %>%
@@ -20,7 +21,10 @@ make_trace_df <- function(my_file_dir, msg = F){
     rowMeans() %>%
     # dplyr::tibble(Time = 1:length(.) * y_px_size / 1000, # set the time in seconds
     #        Fluorescence = .)
-    dplyr::tibble(Fluorescence = .)
+    dplyr::tibble(file_id = file_id,
+                  Fluorescence = .,
+                  y_px_size =  y_px_size)
   
+  rm(file_id, y_px_size)
   return(my_trace_df)
 }
